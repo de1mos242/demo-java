ddca34067507b2354b0f74b748c91a0e0d0e088d
De1mos <de1m0s242@gmail.com>
2016-05-18 03:47:23 +0700

    S05

--- todoAppS01/src/todoAppS01/EmbeddedStart.java
+++ todoAppS01/src/todoAppS01/EmbeddedStart.java
@@ -1,26 +1,24 @@
 package todoAppS01;
 
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class EmbeddedStart {
 
 	public static void main(String[] args) {
 		try {
 			Server server = new Server(8080);
-			
-			ServletContextHandler context = new ServletContextHandler();
-	        context.setContextPath("/");
-	        server.setHandler(context);
 
-	        context.addServlet(new ServletHolder(new EmperorServlet()),"/*");
-	        context.addServlet(new ServletHolder(new TodoServlet()),"/todo");
-	        
+			WebAppContext context = new WebAppContext();
+			context.setDescriptor("/WEB-INF/web.xml");
+			context.setResourceBase(".");
+			context.setContextPath("/");
+			
+			server.setHandler(context);
+
 			server.start();
-	        server.join();
-		}
-		catch (Exception e) {
+			server.join();
+		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
 	}
