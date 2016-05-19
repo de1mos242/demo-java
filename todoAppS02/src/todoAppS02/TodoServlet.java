d32f6afc88b09c7ab7a412fc4ba38c670b2b0d2c
De1mos <de1m0s242@gmail.com>
2016-05-19 08:07:51 +0700

    S07

---
+++ todoAppS02/src/todoAppS02/TodoServlet.java
@@ -0,0 +1,27 @@
+package todoAppS02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class TodoServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        
+		PrintWriter writer = resp.getWriter();
+		resp.getWriter().println("<ul>");
+		Arrays.asList("write simple server", "add stupid handler", "add conditional to handler").stream()
+				.forEach(todo -> writer.println("\t<li>" + todo + "</li>"));
+
+		writer.println("</ul>");
+	}
+}
