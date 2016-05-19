25637beed6f19aa2cc3cd37aadf7fbd709b76bdd
De1mos <de1m0s242@gmail.com>
2016-05-19 07:16:10 +0700

    S06

--- todoAppS01/src/todoAppS01/EmbeddedStart.java
+++ todoAppS01/src/todoAppS01/EmbeddedStart.java
@@ -8,11 +8,12 @@ public class EmbeddedStart {
 	public static void main(String[] args) {
 		try {
 			Server server = new Server(8080);
-
 			WebAppContext context = new WebAppContext();
-			context.setDescriptor("/WEB-INF/web.xml");
+			context.setDescriptor(EmbeddedStart.class.getResource("/WEB-INF/web.xml").toString());
 			context.setResourceBase(".");
 			context.setContextPath("/");
+			
+			System.out.println(String.format("Init: [%s] [%s] [%s]" , context.getDescriptor(), context.getResourceBase(), context.getContextPath()));
 			
 			server.setHandler(context);
 
