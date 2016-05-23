33dec103411bcff534c3196997b8effced55dc4c
De1mos <de1m0s242@gmail.com>
2016-05-24 01:42:14 +0700

    S2-02 - init db from servlet event

---
+++ todoAppS03/src/main/java/todoAppS03/SqlInitListener.java
@@ -0,0 +1,67 @@
+package todoAppS03;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class SqlInitListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		System.out.println("init db from event listener");
+		prepareDBIfNeed();
+		System.out.println("init db from event listener finished");
+	}
+	
+	private void prepareDBIfNeed() {
+		Connection connection = null;  
+	     ResultSet resultSet = null;  
+	     Statement statement = null;  
+
+	     try 
+	     {  
+	         Class.forName("org.sqlite.JDBC");  
+	         connection = DriverManager.getConnection("jdbc:sqlite:/home/denis/todoApp.db");  
+	         statement = connection.createStatement();  
+	         resultSet = statement  
+	                 .executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='todos';");  
+	         if (!resultSet.next()) 
+	         {  
+	        	 statement.execute("create table todos (id bigint, name varchar(255));");
+	        	 
+	        	 statement.execute("insert into todos (id, name) values (1, 'write simple server');");
+	        	 statement.execute("insert into todos (id, name) values (2, 'add stupid handle');");
+	        	 statement.execute("insert into todos (id, name) values (3, 'add conditional to handler');");
+	        	 statement.execute("insert into todos (id, name) values (4, 'build with gradle');");
+	        	 statement.execute("insert into todos (id, name) values (5, 'use sqlite');");
+	         }  
+	     } 
+	     catch (Exception e) 
+	     {  
+	         e.printStackTrace();  
+	     }
+	     finally 
+	     {  
+	         try 
+	         {  
+	             resultSet.close();  
+	             statement.close();  
+	             connection.close();  
+	         } 
+	         catch (Exception e) 
+	         {  
+	             e.printStackTrace();  
+	         }  
+	     }  
+	}
+
+}
--- todoAppS03/src/main/java/todoAppS03/SqliteServlet.java
+++ todoAppS03/src/main/java/todoAppS03/SqliteServlet.java
@@ -18,7 +18,6 @@ public class SqliteServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String idPath = req.getPathInfo().substring(1);
-		prepareDBIfNeed();
 		resp.setContentType("text/html;charset=utf-8");
         resp.setStatus(HttpServletResponse.SC_OK);
         
@@ -26,47 +25,7 @@ public class SqliteServlet extends HttpServlet {
     	writer.println("<h1>" + getTodoById(idPath) + "</h1>");
 	}
 	
-	private void prepareDBIfNeed() {
-		Connection connection = null;  
-	     ResultSet resultSet = null;  
-	     Statement statement = null;  
-
-	     try 
-	     {  
-	         Class.forName("org.sqlite.JDBC");  
-	         connection = DriverManager.getConnection("jdbc:sqlite:/home/denis/todoApp.db");  
-	         statement = connection.createStatement();  
-	         resultSet = statement  
-	                 .executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='todos';");  
-	         if (!resultSet.next()) 
-	         {  
-	        	 statement.execute("create table todos (id bigint, name varchar(255));");
-	        	 
-	        	 statement.execute("insert into todos (id, name) values (1, 'write simple server');");
-	        	 statement.execute("insert into todos (id, name) values (2, 'add stupid handle');");
-	        	 statement.execute("insert into todos (id, name) values (3, 'add conditional to handler');");
-	        	 statement.execute("insert into todos (id, name) values (4, 'build with gradle');");
-	        	 statement.execute("insert into todos (id, name) values (5, 'use sqlite');");
-	         }  
-	     } 
-	     catch (Exception e) 
-	     {  
-	         e.printStackTrace();  
-	     }
-	     finally 
-	     {  
-	         try 
-	         {  
-	             resultSet.close();  
-	             statement.close();  
-	             connection.close();  
-	         } 
-	         catch (Exception e) 
-	         {  
-	             e.printStackTrace();  
-	         }  
-	     }  
-	}
+	
 	
 	private String getTodoById(String todoId) {
 		String resultString = "Not found";
