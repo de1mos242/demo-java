6faaf5565c6a06429197c07fdb6215c43151d84a
De1mos <de1m0s242@gmail.com>
2016-05-27 00:36:43 +0700

    S2-05 - spring jsp controllers

--- todoAppS03/src/main/java/todoAppS03/TodoControllerBean.java
+++ todoAppS03/src/main/java/todoAppS03/TodoControllerBean.java
@@ -4,12 +4,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TodoControllerBean {
 	
-	@RequestMapping("hello")
+	@RequestMapping("/hello")
 	public ResponseEntity<String> helloHandler() {
 		return new ResponseEntity<>("<h1>hello spring</h1>", HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/static-jsp", method = RequestMethod.GET)
+	public String jspHandler() {
+		return "/WEB-INF/site/static.jsp";
+	}
+	
+	@RequestMapping("/model-view-jsp")
+	public ModelAndView mvHandler() {
+		return new ModelAndView("/WEB-INF/site/static.jsp");
 	}
 }
